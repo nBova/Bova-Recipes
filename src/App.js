01209af200ede7b74cbe2fd1283733239f3e84e9
Nick Bova <npb7642@rit.edu>
2024-02-15 02:15:35 -0500

    did some re routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import './App.css';
+import './CSS/App.css';
+import { Routes, Route} from "react-router-dom";
+import Layout from './pages/Layout';
+import Recipes from './pages/Recipes';
+import Blogs from './pages/Blogs';
+import Home from './pages/Home';
 
-function App() { }
+function App() { 
+  return(
+    <>
+    <Layout />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/blogs" element={<Blogs />} />
+    </Routes>
+    </>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import Recipes from "./pages/Recipes"
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="recipes" element={<Recipes />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import { HashRouter as Router } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<Router>
+  <App />
+</Router>
+
+);
